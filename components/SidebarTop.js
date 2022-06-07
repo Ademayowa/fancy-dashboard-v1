08940db231a8f099c86f446da168403dc40a8fe2
Ademayowa <ademayowaadeniyi@gmail.com>
2022-06-07 15:53:28 +0100

    used gap-x-2 in place of space-x-2

--- components/SidebarTop.js
+++ components/SidebarTop.js
@@ -1,6 +1,6 @@
 export default function SidebarTop() {
   return (
-    <div className='px-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg'>
+    <div className='px-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg mx-2'>
       <h1 className='pt-16 font-bold text-white'>Frontend Mentor</h1>
       <p className='text-white font-light pb-8'>Feedback Board</p>
     </div>
